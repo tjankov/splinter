16ee279479f94e526faf61114686825365466699
andrews medina <andrewsmedina@gmail.com>
2010-10-11 16:47:22 -0300

    find methods for mechanize driver

--- splinter/driver/mechanize_driver.py
+++ splinter/driver/mechanize_driver.py
@@ -2,6 +2,7 @@ from lxml.cssselect import CSSSelector
 from splinter.driver import DriverAPI, ElementAPI
 
 import mechanize
+import lxml.html as lhtml
 
 class Mechanize(DriverAPI):
     pass
@@ -25,21 +26,52 @@ class Mechanize(DriverAPI):
         self.driver.open(url)
         self.response = self.driver.response().read()
 
+    def find_by_id(self, id):
+        _html = lhtml.fromstring(self.response)
+        return MechanizeElement(_html.get_element_by_id(id=id))
+
+    def find_by_css_selector(self, css_selector):
+        selector = CSSSelector(css_selector)
+        _html = lhtml.fromstring(self.response)
+        return MechanizeElement(_html.cssselect(css_selector)[0])
+        
+    def find_by_xpath(self, xpath):
+        _html = lhtml.fromstring(self.response)
+        return MechanizeElement(_html.xpath(xpath)[0])
+        
+    def find_by_tag(self, tag):
+        return self.find_by_xpath('//%s' % tag)
+        
+    def find_by_name(self, name):
+        return self.find_by_xpath("*[//@name='%s']" % name)
+
     def find_link_by_href(self, href):
-        return MechanizeElement(self.driver.find_link(url=href))
+        return MechanizeLinkELement(self.driver.find_link(url=href))
 
     def find_link_by_text(self, text):
-        return MechanizeElement(self.driver.find_link(text=text))
+        return MechanizeLinkELement(self.driver.find_link(text=text))
              
     def quit(self):
         self.driver.close()
+        
+class MechanizeLinkELement(ElementAPI):
 
+    def __init__(self, element):
+
+        element.href = element.url
+        self._element = element
+
+    def __getitem__(self, attr):
+        return getattr(self._element, attr)
+    
 class MechanizeElement(ElementAPI):
 
     def __init__(self, element):
-        if isinstance(element, mechanize._html.Link):
-            element.href = element.url
         self._element = element
+    
+    @property
+    def value(self):
+        return self._element.text
 
     def __getitem__(self, attr):
         return getattr(self._element, attr)
