d5625a26ff08f084e8d6a4f1bd1132f42638b2e1
Hugo Lopes Tavares <hltbra@gmail.com>
2010-10-22 11:36:56 -0200

    little refactoring: changed ifs to use a dict

--- splinter/browser.py
+++ splinter/browser.py
@@ -3,11 +3,12 @@ from splinter.driver.mechanize_driver import Mechanize
 
 class Browser(object):
 
+    _DRIVERS = dict(mechanize=Mechanize,
+                    webdriver=WebDriver)
+
     def __init__(self, driver='webdriver'):
-        if driver == 'mechanize':
-            self._driver = Mechanize()
-        if driver == 'webdriver':    
-            self._driver = WebDriver()
+        driver_class = self._DRIVERS[driver]
+        self._driver = driver_class()
 
     def __getattr__(self, attr):
         return getattr(self._driver, attr)
