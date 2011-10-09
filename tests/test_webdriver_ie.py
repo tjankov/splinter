b03ce204735d66c92db29db0c87992493e6265d0
Francisco Souza <f@souza.cc>
2011-10-09 13:48:35 -0300

    Removed and stopped using Namespace class

--- tests/test_webdriver_ie.py
+++ tests/test_webdriver_ie.py
@@ -8,24 +8,16 @@ except ImportError:
 from base import WebDriverTests
 from fake_webapp import EXAMPLE_APP
 from splinter.browser import Browser
-from tests import Namespace
-
-ns = Namespace()
-
-
-def setUpModule():
-    ns.browser = Browser('ie')
-
-
-def tearDownModule():
-    ns.browser.quit()
 
 
 class IEBrowserTest(WebDriverTests, unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.browser = ns.browser
+        cls.browser = Browser('ie')
+
+    def tearDownClass(cls):
+        cls.browser.quit()
 
     def setUp(self):
         self.browser.visit(EXAMPLE_APP)
